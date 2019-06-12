094a210fcef515c66de257f8627fefafdb8a16e1
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-12 15:19:24 +0300

    add test for gather

--- tests/Unit/MetaTest.php
+++ tests/Unit/MetaTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Services\MetaServiceInterface;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,6 +20,13 @@ class MetaTest extends TestCase
         $this->assertArrayHasKey('page_max_size', $data);
         $this->assertArrayHasKey('file_size', $data);
         $this->assertArrayHasKey('updated_at', $data);
+    }
 
+    public function testGatherPutCacheItem()
+    {
+        $service = resolve(MetaServiceInterface::class);
+
+        Cache::shouldReceive('put')->once();
+        $service->gather();
     }
 }
