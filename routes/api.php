0ffa92ce1bb4f88eb107fe7e446d19424d964e9c
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-13 13:07:39 +0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'throttle:360,1'], function() {
+
+    Route::apiResource('records', 'RecordsController');
+
+    Route::post('exchange', 'ExchangeController@import');
+
+    Route::get('meta', 'MetaController@show');
+
 });
